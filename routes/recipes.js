1d1b18666af0f83c85f9bdcbefcb1c675dc6165c
virendra34 <yvirendra34@gmail.com>
2021-07-19 12:36:51 +0530

    added comments to explain logic

--- routes/recipes.js
+++ routes/recipes.js
@@ -35,6 +35,7 @@ router.get('/test', (req, res) => {
 	let finalString = '';
 	let temp = '';
 	for (let i = 0; i < chars.length; i++) {
+		// step1: if s[i] is lowercase and s[i+1] is uppercase then exchange them and add * at s[i+2] position
 		if (i < chars.length - 1 && chars[i] !== '*' && chars[i + 1] !== '*' && isNaN(chars[i]) && isNaN(chars[i + 1])) {
 			if (chars[i] === chars[i].toLowerCase() && chars[i + 1] === chars[i + 1].toUpperCase()) {
 				temp = chars[i];
@@ -43,6 +44,7 @@ router.get('/test', (req, res) => {
 				chars.splice(i + 2, 0, '*');
 			}
 		}
+		// step2: if there ar any number at any position replace them with 0 and move that number at front
 		if (parseInt(chars[i]) >= 1 && parseInt(chars[i]) <= 9) {
 			temp = chars[i];
 			chars[i] = '0';
@@ -55,13 +57,16 @@ router.get('/test', (req, res) => {
 	// remove unwanted chars from string except numbers
 	chars = encryptedString.split('').reverse();
 	for (let i = 0; i < chars.length; i++) {
+		// step1: remove * from the encrypted string
 		if (chars[i] === '*') {
 			chars.splice(i, 1);
 		}
+		// step2: replace 0 with the right most number in the string
 		if (parseInt(chars[i]) === 0) {
 			chars[i] = chars[chars.length - 1];
 			chars.splice(chars.length - 1, 1);
 		}
+		// step3: if s[i] is uppercase and s[i+1] is lowercase then exchange them
 		if (i < chars.length - 1 && isNaN(chars[i]) && isNaN(chars[i + 1])) {
 			if (chars[i + 1] === chars[i + 1].toUpperCase() && chars[i] === chars[i].toLowerCase()) {
 				temp = chars[i + 1];
